664377469ea2b7e6e5ea6c8810569aa41639e1cc
Chidi Nwanneka <cnwanneka@gmail.com>
2023-02-20 14:39:20 +0000

    added javascript code for retrieving API data

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 
+// Logic for retrieving data from API.
 
-fetch("https://api.openweathermap.org/geo/1.0/direct?q=London,GBR&limit=5&appid=9912a871379b3e7285fcdbca65faf901")
+fetch("https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=9912a871379b3e7285fcdbca65faf901")
+    .then(response => response.json())
+    .then(citiesFound => {
+        let firstCity = citiesFound[0];
+        console.log(firstCity.lat);
+        console.log(firstCity.lon);
+
+        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=9912a871379b3e7285fcdbca65faf901`)
+    })
+
+
     .then(response => response.json())
     .then(data => {
 
-        console.log(data)
+        console.log(data);
     })
 
-    
+
+  
